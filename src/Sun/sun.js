6642f1fc746b958685662bb741533cc2a3e6e44e
Olivier Lefebvre <lefebvre.olivier11@gmail.com>
2024-12-18 00:31:30 +1100

    Better calculation of sun's bands

--- src/Sun/sun.js
+++ src/Sun/sun.js
@@ -1,113 +1,120 @@
 class Sun {
-    #redColor = color(255, 0, 0);
-  
-    constructor(depth, radius = 50, speed = 0.05, yLimit = 60) {
-      this.depth = depth;
-      this.radius = radius;
-      this.speed = speed;
-      this.yLimit = yLimit;
-  
-      this.direction = 1;
-      this.pos = -yLimit;
-      this.#calculateBands();
+  #red = color(255, 0, 0);
+  #yellow = color(255, 255, 0);
 
-      console.log(this.bands);
+  constructor(depth, radius = 50, speed = 0.05, yLimit = 60) {
+    this.depth = depth;
+    this.radius = radius;
+    this.diameter = radius * 2;
+    this.speed = speed;
+    this.yLimit = yLimit;
+
+    this.direction = 1;
+    this.pos = -yLimit;
+    this.bandsBaseline = radius;
+    this.#calculateBands();
+
+    console.log(this.bands);
+  }
+
+  #calculateBands() {
+    this.bands = [];
+
+    let bandWidth = 2;
+    let bandCenter = -this.radius;
+    let bandSpacing = 7;
+    let bandTop = bandCenter - bandWidth / 2.0;
+    let bandBottom = bandCenter + bandWidth / 2.0;
+
+    while (bandTop < this.radius) {
+      this.bands.push(new Band(bandTop, bandBottom));
+
+      bandWidth += 1;
+      bandSpacing -= 1;
+
+      bandTop = bandBottom + bandSpacing;
+      bandBottom = bandTop + bandWidth;
     }
+  }
 
-    #calculateBands(){
-      this.bands = [];
-      
-      let bandWidth = 2;
-      let bandCenter = -this.radius;
-      let bandSpacing = 7;
-      let bandTop = bandCenter - bandWidth / 2.0;
-      let bandBottom = bandCenter + bandWidth / 2.0;
+  #calculateNewState() {
+    this.#calculateNewPosition();
+    this.#calculateNewBorder();
+    this.#calculateNewColor();
+  }
 
-      while(bandTop < 0){
-        this.bands.push(new Band(bandTop, bandBottom));
+  #calculateNewPosition() {
+    const x = sigmoid(
+      map(this.pos - this.yLimit / 6.0, -this.yLimit, this.yLimit, -4, 4)
+    );
 
-        bandWidth += 1;
-        bandSpacing -= 1;
+    const delta = x * this.direction * this.speed * deltaTime;
 
-        bandTop = bandBottom + bandSpacing;
-        bandBottom = bandTop + bandWidth;
-      }
-    }
-  
-    #calculateNewPosition() {
-      let x = sigmoid(
-        map(this.pos - this.yLimit / 6.0, -this.yLimit, this.yLimit, -4, 4)
-      );
-      this.pos += x * this.direction * this.speed * deltaTime;
-  
-      if (this.pos > this.yLimit && this.direction > 0) this.direction = -1;
-      else if (this.pos < -this.yLimit && this.direction < 0) this.direction = 1;
-    }
+    this.pos += delta;
+    this.bandsBaseline -= delta;
 
-    #isInBands(h){
-      for (let i = 0; i < this.bands.length; i++)  {
-        if(this.bands[i].isIn(h))
-          return true;
-      }
-      return false;
-    }
-  
-    #drawContour() {
-      let startAngle;
-      let endAngle;
-      if (this.pos + this.radius > 0) {
-        const angle = Math.asin(-this.pos / this.radius);
-        startAngle = PI - angle;
-        endAngle = angle;
-      } else {
-        startAngle = 0;
-        endAngle = 2 * PI;
-      }
-  
-      strokeWeight(1);
-      stroke(this.#redColor);
-      arc(
-        0,
-        this.pos,
-        this.radius * 2,
-        this.radius * 2,
-        startAngle,
-        endAngle,
-        OPEN,
-        50
-      );
-    }
-  
-    #fill() {
-      const c2 = color(255, 255, 0);
-      const amt = map(-this.pos, 0, this.yLimit, 0, 1);
-      const c3 = lerpColor(this.#redColor, c2, amt);
-  
-      stroke(c3);
-      const sunTop = this.pos - this.radius + 0.2;
-      const sunBottom = this.pos + this.radius + 0.2;
-  
-      let x;
-      for (let y = min(sunTop, 0); y < min(sunBottom, 0); y += 0.5) {
-        //console.log(`Y:${y}, IsInBands:${this.#isInBands(y)}`)
-        if(this.#isInBands(y))
-          continue;
+    if (this.pos > this.yLimit && this.direction > 0) this.direction = -1;
+    else if (this.pos < -this.yLimit && this.direction < 0) this.direction = 1;
+  }
 
-        x = findXCoordFromCircle(y, this.radius, this.pos);
-        line(-x, y, 0, x, y, 0);
-      }
-    }
-  
-    draw() {
-      this.#calculateNewPosition();
-      if (this.pos - this.radius > 0) {
-        return;
-      }
-  
-      push();
-      this.#drawContour();
-      this.#fill();
-      pop();
+  #calculateNewBorder() {
+    if (this.pos + this.radius > 0) {
+      const angle = Math.asin(-this.pos / this.radius);
+      this.startAngle = PI - angle;
+      this.endAngle = angle;
+    } else {
+      this.startAngle = 0;
+      this.endAngle = 2 * PI;
     }
   }
   
+  #calculateNewColor() {
+    const amt = map(-this.pos, -this.yLimit / 2.0, this.yLimit, 0, 1);
+    this.mainColor = lerpColor(this.#red, this.#yellow, amt);
+  }
+
+  #drawContour(fillColor) {
+    strokeWeight(1);
+    fill(fillColor);
+    arc(
+      0,
+      this.pos,
+      this.diameter,
+      this.diameter,
+      this.startAngle,
+      this.endAngle,
+      OPEN,
+      50
+    );
+  }
+
+  #fill() {
+    push();
+    beginClip();
+    this.bands
+      .filter((b) => b.bottom + this.bandsBaseline + b.width < 0)
+      .forEach((b) => {
+        rect(
+          -this.radius,
+          b.bottom + this.bandsBaseline,
+          this.diameter,
+          b.width
+        );
+      });
+    endClip();
+    this.#drawContour(this.#red);
+    pop();
+  }
+
+  draw() {
+    this.#calculateNewState();
+    if (this.pos - this.radius > 0) {
+      return;
+    }
+
+    push();
+    this.#drawContour(this.mainColor);
+    this.#fill();
+    pop();
+  }
+}
